21b092bbf8fa787084647c0cca31824920ed96ac
marihachi <marihachi0620@gmail.com>
2015-08-18 17:01:54 +0900

    add Misskey and MisskeyRequest

---
+++ src/MSharp/Misskey.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Json;
+
+namespace MSharp
+{
+	class Misskey
+	{
+		public string AppKey { private set; get; }
+		public string UserKey { private set; get; }
+		public string UserId { private set; get; }
+		private string AuthenticationSessionKey { set; get; }
+
+		public Misskey(string appKey)
+		{
+			this.AppKey = appKey;
+		}
+
+		public Misskey(string appKey, string userKey, string userId)
+		{
+			this.AppKey = appKey;
+			this.UserKey = userKey;
+			this.UserId = userId;
+		}
+
+		public async Task StartAuthorize()
+		{
+			var ret = await new MisskeyRequest(this, HttpRequest.MethodType.GET, "sauth/get-authentication-session-key").Request();
+
+			var json = JsonObject.Parse(ret).AsDynamic();
+			this.AuthenticationSessionKey = json.authenticationSessionKey;
+
+			System.Diagnostics.Process.Start("https://api.misskey.xyz/authorize@" + this.AuthenticationSessionKey);
+		}
+
+		public async Task<Misskey> AuthorizePIN(string pinCode)
+		{
+			var ret = await new MisskeyRequest(this, HttpRequest.MethodType.GET, "sauth/get-user-key",
+				new Dictionary<string, string> {
+					{ "authentication-session-key", AuthenticationSessionKey },
+					{ "pin-code", pinCode}
+				}).Request();
+
+			var json = JsonObject.Parse(ret).AsDynamic();
+
+			return new Misskey(this.AppKey, json.userId, json.userId);
+		}
+	}
+}
---
+++ src/MSharp/MisskeyRequest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace MSharp
+{
+	class MisskeyRequest
+	{
+		public string BaseUrl { private set; get; }
+
+		private HttpRequest RequestInstance { private set; get; }
+
+		public MisskeyRequest(
+			Misskey misskey,
+			HttpRequest.MethodType method,
+			string endPoint,
+			Dictionary<string, string> parameters = null, string baseUrl = null)
+		{
+			this.BaseUrl = baseUrl ?? "https://api.misskey.xyz/";
+
+			var match = Regex.Match(endPoint, "^/?(.+)$");
+			endPoint = match.Groups[1].ToString();
+
+			var headers = new Dictionary<string, string> {
+				{ "sauth-app-key", misskey.AppKey }
+			};
+
+			this.RequestInstance = new HttpRequest(method, BaseUrl + endPoint, headers, parameters);
+		}
+
+		public async Task<string> Request()
+		{
+			return await this.RequestInstance.Request();
+		}
+	}
+}
