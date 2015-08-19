a3deb4ca626615291462c6b6bd2c240b720cb759
marihachi <marihachi0620@gmail.com>
2015-08-20 02:41:08 +0900

    add MSharpException

---
+++ src/MSharp/Core/MSharpException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MSharp.Core
+{
+	/// <summary>
+	/// MSharpの全般的なエラーを表します。
+	/// </summary>
+	public class MSharpException : ApplicationException
+	{
+		public MSharpException(string message)
+			: base(message) { }
+
+		public MSharpException(string message, Exception innerException)
+			: base(message, innerException) { }
+	}
+}
