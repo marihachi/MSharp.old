229db2fc080ef251819711ec5ecf0db6f2af6c74
marihachi <marihachi0620@gmail.com>
2015-08-21 05:20:32 +0900

    fix

---
+++ src/MSharp/API/EndPoints.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MSharp.API
+{
+	/// <summary>
+	/// エンドポイントの一覧を表します。
+	/// </summary>
+	public static class EndPoints
+	{
+
+	}
+}
